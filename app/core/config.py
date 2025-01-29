817bb5d0c581e65d54d648546917cebed1cf5ea9
Romihia <118122586+Romihia@users.noreply.github.com>
2025-01-29 22:57:10 +0200

    update .env

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,13 @@ import os
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb://localhost:27017"
-    MONGO_DB_NAME: str = "bee_vision_db"
+    MONGO_URI = os.environ.get("MONGO_URI")
+    MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME')
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_BUCKET_NAME = os.environ.get("AWS_S3_BUCKET_NAME")
+    AWS_S3_REGION = os.environ.get("AWS_S3_REGION")
+
 
     class Config:
         env_file = ".env"
--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -1,31 +1,70 @@
 from fastapi import APIRouter, WebSocket, UploadFile, File, HTTPException
 import cv2
 import numpy as np
+import time
+import os
+import boto3
 from starlette.websockets import WebSocketDisconnect
+from app.core.config import settings
+
 
 router = APIRouter()
+def upload_to_s3(file_path):
+    """
+    upLoad to S3
+    """
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION
+    )
+
+    bucket_name = settings.AWS_S3_BUCKET_NAME
+    s3_key = f"data_bee/{os.path.basename(file_path)}"
+
+    try:
+        s3.upload_file(file_path, bucket_name, s3_key)
+        print(f"Upload Successful: {s3_key}")
+        os.remove(file_path)  # מחיקת הקובץ לאחר ההעלאה
+    except Exception as e:
+        print(f"Failed to upload video: {e}")
 
 @router.websocket("/live-stream")
 async def live_stream(websocket: WebSocket):
+ 
     print("Attempting to accept WebSocket connection")
     await websocket.accept()
+
+    timestamp = int(time.time())
+    video_filename = f"./temp_videos/stream_{timestamp}.mp4"
+    os.makedirs("./temp_videos", exist_ok=True)
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    frame_width, frame_height = 640, 480
+    fps = 20.0
+    video_writer = cv2.VideoWriter(video_filename, fourcc, fps, (frame_width, frame_height))
+
     try:
         while True:
-            # קבלת פריים (Frame) מה-Frontend
             data = await websocket.receive_bytes()
-
-            # המרה ל-Numpy Array ו-OpenCV Frame
             nparr = np.frombuffer(data, np.uint8)
             frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-            process_frame(frame)
+            if frame is not None:
+                video_writer.write(frame)
 
     except WebSocketDisconnect:
-        print("WebSocket disconnected")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        print("WebSocket disconnected. Saving and uploading video...")
+        video_writer.release()
+        upload_to_s3(video_filename)
 
-# API להעלאת וידאו
+    except Exception as e:
+        video_writer.release()
+        print(f"Error during streaming: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
 @router.post("/upload")
 async def upload_video(file: UploadFile = File(...)):
     """
