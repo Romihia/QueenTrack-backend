14ab7605bdb973e5bf4c494c937c0c899d721073
maxim2100 <maxsim2100@gmail.com>
2025-06-03 16:54:35 +0300

    Removed AWS config, added local storage config

--- app/core/config.py
+++ app/core/config.py
@@ -5,11 +5,8 @@ class Settings(BaseSettings):
     MONGO_URI = os.environ.get('MONGO_URI')
     MONGO_DB_NAME = os.environ.get('MONGO_DB_NAME')
     
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET_NAME')
-    AWS_S3_REGION = os.environ.get('AWS_S3_REGION')
-    print("AWS_ACCESS_KEY_ID",AWS_ACCESS_KEY_ID)
+    # Local video storage configuration
+    VIDEOS_DIR = os.environ.get('VIDEOS_DIR', '/data/videos')
 
     class Config:
         env_file = ".env"
